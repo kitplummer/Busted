d45e9bbb139810520667aa5c37382317cf75d193
Kit Plummer <kplummer@dozer.local>
2010-06-24 10:13:04 -0700

    adding depends, testing.

--- busted.rb
+++ busted.rb
@@ -1,12 +1,24 @@
 require 'rubygems'
 require 'sinatra'
+require 'httparty'
+require 'builder'
 
 get '/' do 
 	"Hello from Busted, the SunTran WebWatch Feed Proxy!"
 end
 
 get '/route/:id' do |route|
-  "Fetching route: #{route}"
+  "Fetching all information for route: #{route}"
 end
 
+get '/route/:id/stations' do |route|
+  "Fetching all station information for route: #{route}"
+end
 
+get '/route/:id/stops' do |route|
+  "Fetching all stop (including stations) information for route: #{route}"
+end
+
+get '/route/:id/busses' do |route|
+  "Fetching all bus information for route: #{route}"
+end
