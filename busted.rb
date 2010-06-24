ef854b5e4f8730ad1f0458d61afee84a43a5e8ed
Kit Plummer <kplummer@dozer.local>
2010-06-24 09:50:51 -0700

    sinatra skeleton

---
+++ busted.rb
@@ -0,0 +1,11 @@
+require 'rubygems'
+require 'sinatra'
+
+get '/' do 
+	"Hello from Busted, the SunTran WebWatch Feed Proxy!"
+end
+
+get "/route/"
+end
+
+
---
+++ config.ru
@@ -0,0 +1,3 @@
+require 'busted'
+
+run Sinatra::Application
